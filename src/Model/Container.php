f56a06a1a6b12167881cdbe5681679cebab0be26
fmontes <751424+fmontes@users.noreply.github.com>
2025-03-04 16:19:53 -0600

    refactor(model): Remove identifier and uuid from Container model
    Simplify Container model by removing unnecessary identifier and uuid properties, focusing on core container structures and rendering

--- src/Model/Container.php
+++ src/Model/Container.php
@@ -9,14 +9,10 @@ use Symfony\Component\Serializer\Annotation as Serializer;
 class Container implements \JsonSerializable
 {
     /**
-     * @param string $identifier The container identifier
-     * @param string $uuid The container UUID
      * @param array $containerStructures Container structure details
      * @param array $rendered Rendered content
      */
     public function __construct(
-        public readonly string $identifier,
-        public readonly string $uuid,
         public readonly array $containerStructures = [],
         public readonly array $rendered = [],
     ) {
@@ -30,8 +26,6 @@ class Container implements \JsonSerializable
     public function jsonSerialize(): array
     {
         return [
-            'identifier' => $this->identifier,
-            'uuid' => $this->uuid,
             'containerStructures' => $this->containerStructures,
             'rendered' => $this->rendered,
         ];
