010d210ba5beae22c56ccaa6a1f2d724910f023e
Freddy Montes <freddymontes@gmail.com>
2025-04-09 14:54:34 -0600

    feat(dotcms): Add dotUVE subscription for real-time updates in app.js

--- examples/dotcms-laravel/resources/js/app.js
+++ examples/dotcms-laravel/resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+// dotUVE is a global variable that is set by the dotCMS UVE JavaScript API
+if (window.dotUVE) {
+    window.dotUVE.createUVESubscription('changes', (changes) => {
+        window.location.reload();
+    })
+} else {
+    console.warn('dotUVE is not available, you might experience issues with the the Universal Visual Editor');
+}
--- examples/dotcms-symfony/assets/app.js
+++ examples/dotcms-symfony/assets/app.js
@@ -8,3 +8,12 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// dotUVE is a global variable that is set by the dotCMS UVE JavaScript API
+if (window.dotUVE) {
+    window.dotUVE.createUVESubscription('changes', (changes) => {
+        window.location.reload();
+    })
+} else {
+    console.warn('dotUVE is not available, you might experience issues with the the Universal Visual Editor');
+}
