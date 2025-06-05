f7ad4fda7db775ede4012b900b39cbf12366c8b5
Jamie Mauro <jameson.mauro@dotcms.com>
2025-06-05 10:54:43 -0400

    updating lock files and related, to ensure proper versioning

--- examples/dotcms-laravel/resources/views/dotcms/components/navigation.blade.php
+++ examples/dotcms-laravel/resources/views/dotcms/components/navigation.blade.php
@@ -4,12 +4,12 @@
         @if(isset($navigation) && $navigation->hasChildren())
             @foreach($navigation->children as $item)
                 <li>
-                    <a href="{{ $item->href ?? $item['href'] }}" target="{{ $item->target ?? $item['target'] }}">{{ $item->title ?? $item['title'] }}</a>
+                    <a href="{{ $item->href ?? $item['href'] }}" target="{{ $item->target ?? $item['target'] }}">{{ Str::title($item->title ?? $item['title']) }}</a>
                     @if($item->hasChildren())
                         <ul class="flex gap-4 mt-2">
                             @foreach($item->children as $child)
                                 <li>
-                                    <a href="{{ $child->href ?? $child['href'] }}" target="{{ $child->target ?? $child['target'] }}">{{ $child->title ?? $child['title'] }}</a>
+                                    <a href="{{ $child->href ?? $child['href'] }}" target="{{ $child->target ?? $child['target'] }}">{{ Str::title($child->title ?? $child['title']) }}</a>
                                 </li>
                             @endforeach
                         </ul>
